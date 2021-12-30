88b59e8a93c6c793414e463b5d8b363ec4765fe1
borayil <borayil00@gmail.com>
2021-12-30 20:35:10 +0300

    Scales RGB alpha based on lifespan

--- sketch.js
+++ sketch.js
@@ -3,17 +3,32 @@ function windowResized() {
   background(35);
 }
 
-function setColor(pos) {
-  let r = map(mouseX, 0, windowWidth, 50, 255)
-  let g = map(mouseY, 0, windowHeight, 255, 50)
-  let b = map(mouseX, 0, windowWidth, 50, 255)
-  fill(r,g,b)
+function setColor(v) {
+  let r = map(v.pos.x, 0, windowWidth, 50, 255)
+  let g = map(v.pos.y, 0, windowHeight, 255, 50)
+  let b = map(v.pos.x, 0, windowWidth, 50, 255)
+  let stroke_color = color(r, g, b, lifespanWeight(v))
+  stroke(stroke_color)
+}
 
+function lifespanWeight(v) {
+  
+  // Growing
+  if (v.lifespan <= MAX_LIFESPAN / 2) {
+      return map(v.lifespan, MIN_LIFESPAN, MAX_LIFESPAN / 2, 0, 1000)
+  } 
+  // Dying
+  else {
+      return map(v.lifespan, MAX_LIFESPAN / 2, MAX_LIFESPAN, 1000, 0)
+  }
+
+  
 }
 
 
+
 const speed = 1
-const ANGLE_CHANGE_SCALE = 0.005// Intensity of angle changes for all vectors
+var angle_change_scale = 0.0045// Intensity of angle changes for all vectors
 var vectors = []
 
 
@@ -21,7 +36,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(35);
   angleMode(DEGREES)
-  noiseDetail(1)
+  noiseDetail(0.05)
+  strokeWeight(1.5)
+
   
   // Grid params
   var density = 100
@@ -37,38 +54,29 @@ function setup() {
 }
 
 function draw() {
-  background(20, 20)
-  noStroke()
+  background(20, 40)
+  
   
   for(let i = 0; i < vectors.length; i++) {
+    
     let v = vectors[i]
     
     // Angle between Perlin noise field and vector
-    let angle = map(noise(v.pos.x*ANGLE_CHANGE_SCALE, v.pos.y*ANGLE_CHANGE_SCALE), 0, 1, 0, 720)
+    let angle = map(noise(v.pos.x*angle_change_scale, v.pos.y*angle_change_scale), 0, 1, 0, 720)
 
-    // Angle between vector and mouse
-    let angle_mouse = atan2((v.pos.y - mouseY), (v.pos.x - mouseX)) * 180 / PI 
-
-    // Add (or sub) 90 deg to this angle
-    angle_mouse -= 270
-
-
+   
+    // To make the field itself move, alternate this scale
+   
+    angle_change_scale -= 0.0000000005
     
     let field_force = createVector(cos(angle), sin(angle))
-    let mouse_force = createVector(cos(angle_mouse), sin(angle_mouse))
-
-    // Weigh movement by the inverse of the distance between vector particle and rotator point
-    
-    // Distance to rotator point (mouse)
-    let dist_mouse = v.pos.dist(mouse_force)
-    mouse_force.mult((1 + 1/dist_mouse) * 20)
-  
-
-    v.pos.add(mouse_force)
+    v.pos.add(field_force)
     
     
-    setColor(v.pos)
+    
     if (v.done()) v.restart(windowWidth, windowHeight)
+    setColor(v)
     v.show()
+    
   }
 }
